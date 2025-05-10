c3d0db39dce715d1db1f11229ec8dfeb71b4990d
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2025-05-10 12:35:22 +0100

    Support validation for literals

--- src/validate/dataclass.py
+++ src/validate/dataclass.py
@@ -1,5 +1,6 @@
 import dataclasses
 import abc
+import typing
 
 
 class Validator(abc.ABC):
@@ -7,8 +8,15 @@ class Validator(abc.ABC):
         failed_validations = []
         for field in dataclasses.fields(self):
             value = getattr(self, field.name)
-            if not isinstance(value, field.type):
-                failed_validations.append(f"{field.name}: \n Expected: {field.type} \n Received: {type(value)}\n")
+
+            if field.type.__class__ == typing._LiteralGenericAlias:
+                # Handle Literal types
+                if value not in field.type.__args__:
+                    failed_validations.append(f"{field.name}: \n Expected: {field.type.__args__} \n Received: {value}\n")
+
+            else:
+                if not isinstance(value, field.type):
+                    failed_validations.append(f"{field.name}: \n Expected: {field.type} \n Received: {type(value)}\n")
 
         if failed_validations:
             raise TypeError(f"Validation failed for {self.__class__.__name__}: \n" + "".join(failed_validations))
--- tests/test_dataclass.py
+++ tests/test_dataclass.py
@@ -1,17 +1,26 @@
 import dataclasses
+from typing import Literal
+
 from src.validate.dataclass import Validator
 
 
+@dataclasses.dataclass
+class TestModel(Validator):
+    id: int
+    name: str
+    description: Literal["test1", "test2", "test3"]
+
+
 def test_dataclass_validator():
-
-    @dataclasses.dataclass
-    class TestModel(Validator):
-        id: int
-        name: str
-        description: str
-
-    model = TestModel(id=1, name="Example", description="This is an example.")
+    model = TestModel(id=1, name="Example", description="test1")
     assert isinstance(model, Validator)
-    assert type(model.id) == int
-    assert type(model.name) == str
-    assert type(model.description) == str
+
+
+def test_dataclass_validator__invalid_literal():
+    try:
+        TestModel(id=1, name="Example", description="test4")
+    except TypeError as e:
+        assert str(e) == (
+            "Validation failed for TestModel: \n"
+            "description: \n Expected: ('test1', 'test2', 'test3') \n Received: test4\n"
+        )
