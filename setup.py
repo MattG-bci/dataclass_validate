414c26c71e311524ad170b9b40c67d1cc2bdcad0
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2025-06-25 23:59:30 +0100

    Change names in the setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-validate',
-    version='0.1',
-    author='Mateusz Grzybowski',
-    description='A simple library for validating Python objects',
-    long_description=open('README.md').read(),
+    name="dataclass-validate",
+    version="0.1",
+    author="Mateusz Grzybowski",
+    description="A simple library for validating Python dataclasses",
+    long_description=open("README.md").read(),
     url="",
     license="",
     packages=find_packages(),
-    install_requires=[""]
+    install_requires=[""],
 )
